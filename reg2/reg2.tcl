e452ca4d229a4f6318502ec57a4bac2da39ae376
LucasFHDortmund <lucas.schreiter034@stud.fh-dortmund.de>
2023-10-13 10:48:43 +0200

    update

--- reg2/reg2.tcl
+++ reg2/reg2.tcl
@@ -2,10 +2,14 @@
 
 # Either 3 or 1
 set countVotingcells 1
+set checkTripledResetDomain 0
+set checkTripledClockDomain 0
 
 proc get_triplicated_and_not_triplicated {leafcells triplicated not_triplicated triplication_faults} {
 	
 	global countVotingcells
+    global checkTripledResetDomain
+    global checkTripledClockDomain
 	
 	#Creates and opens files in working directory
 	#Truncate to zero if it exists. If it does not exist, create a new file. 
@@ -14,20 +18,60 @@ proc get_triplicated_and_not_triplicated {leafcells triplicated not_triplicated 
 	set triplication_faults [open $triplication_faults w]
 
     foreach leafcell $leafcells {
+        # Determine suffix of leafcell
+        regexp {([A-C])_reg(?!.*_)} $leafcell -> leafcell_s
+        
         # Determine output pin
-        set pin [get_pins -of_objects [get_cells $leafcell] -filter {DIRECTION == OUT}]
+        set pin_q [get_pins -of_objects [get_cells $leafcell] -filter {REF_PIN_NAME == Q && DIRECTION == OUT}]
         # Determine net connected to output pin
-        set net [get_nets -of_objects [get_pins $pin]]
+        set net_q [get_nets -of_objects [get_pins $pin_q]]
+        
+        # Determine clock pin
+        set pin_c [get_pins -of_objects [get_cells $leafcell] -filter {DIRECTION == IN && IS_CLOCK == true}]
+        # Determine net connected to clock pin
+        set net_c [get_nets -of_objects [get_pins $pin_c]]
+        # Determine clock net suffix
+        regexp {.*([A-C])} $net_c -> net_c_s
+        # Determine parent of clocking net
+        set parent_net_c [get_property PARENT [get_nets $net_c]]
+        # Determine parent clock net suffix
+        regexp {.*([A-C])} $parent_net_c -> parent_net_c_s
+        
+        # Determine rst pin
+        set pin_rst [get_pins -of_objects [get_cells $leafcell] -filter {DIRECTION == IN && IS_RESET == true}]
+        # Determine net connected to rst pin
+        set net_rst [get_nets -of_objects [get_pins $pin_rst]]
+        # Determine rst net suffix
+        regexp {.*([A-C])} $net_rst -> net_rst_s
+        # Determine parent of reset net
+        set parent_net_rst [get_property PARENT [get_nets $net_rst]]
+        # Determine parent reset net suffix
+        regexp {.*([A-C])} $parent_net_rst -> parent_net_rst_s
+
         # Determine parent reference name
         if {[catch {set name [get_property ORIG_REF_NAME [get_property PARENT [get_cells $leafcell]]]} errorstring]} {
 			set name [lindex [find_top] 0]
 		}
-        # Determine all pins connected to the output net
-        set pins [get_pins -quiet -of_objects [get_nets $net]]
-        # Determine cells that belong to pins
-        set cells [get_cells -quiet -of_objects [get_pins $pins]]
+
+        # TODO introduce better checking of missing voter cell with catch
+
+        # Determine cells that are connected to pins
+        set cells [get_cells -quiet -of_objects [get_nets $net_q]]
         # Determine voting cell, if any
         set voter [get_cells $cells -quiet -filter {NAME =~ "*Voter*"}]
+
+        if {$parent_net_rst_s eq $net_rst_s && $checkTripledResetDomain} {
+            puts "reset net tripled"
+        } else {
+            puts $triplication_faults "$name, $leafcell, $net_rst"
+        }
+        
+        
+        if {$parent_net_c_s eq $net_c_s && $checkTripledClockDomain} {
+            puts "clock net tripled"
+        } else {
+            puts $triplication_faults "$name, $leafcell, $net_clk"
+        }
 
         if {([llength $voter] == 3 && $countVotingcells == 3) || ($voter ne "" && $countVotingcells == 1)} {
             if {[llength $cells] - [llength $voter] == 1} {
@@ -36,7 +80,7 @@ proc get_triplicated_and_not_triplicated {leafcells triplicated not_triplicated 
                 puts $triplicated_log "$name, $leafcell"
             } else {
                 puts "$leafcell has too many output connections, check logs for further details"
-                puts $not_triplicated_log "$name, $leafcell, \[[join $cells ", "]\]"   
+                puts $triplication_faults "$name, $leafcell, \[[join $cells ", "]\]"   
             }  
         } else {
             # If not, add to "not_triplicated"
@@ -55,6 +99,7 @@ proc get_triplicated_and_not_triplicated {leafcells triplicated not_triplicated 
     }
 	close $triplicated_log
 	close $not_triplicated_log
+	close $triplication_faults
 }
 
 #Opens Elaborated Design
