6f8cef6dbcb74cf85827b4b26977b37692dae977
LucasFHDortmund <78490283+LucasFHDortmund@users.noreply.github.com>
2024-02-20 22:54:53 +0100

    Create main.py

---
+++ cdc_rdc_tb_generator/main.py
@@ -0,0 +1,89 @@
+import re
+
+
+def load_file(filepath):
+    with open(filepath) as f:
+        lines = f.read().splitlines()
+    # Remove empty strings
+    lines = [line.strip() for line in lines if line.strip()]
+    return lines
+
+
+def sort_signals(topmodule, signals):
+    sorted_signals = {
+        "A": [],
+        "B": [],
+        "C": []
+    }
+    for signal in signals:
+        signal = signal.split("/")
+        addendum = re.search(r"[ABC]$", signal[-1])
+        if addendum:
+            sorted_signals[addendum.group(0)].append(topmodule + "." + ".".join(signal))
+        else:
+            print(f"Error in: {topmodule}.{".".join(signal)}")
+    return sorted_signals
+
+
+def create_assertion(sorted_signals):
+    for key in sorted_signals.keys():
+        # Loop over copy of sorted_signals
+        for i, signal in enumerate(sorted_signals[key][:]):
+            sorted_signals[key][i] = (
+                f"assert({signal})\n"
+                f"\t\telse\n"
+                f"\t\t\t$fdisplay(this.rdc_fail, \"{signal}\");"
+            )
+    return sorted_signals
+def create_tb_class(assertions):
+    template = f"""
+class rdc;
+    int rdc_success ,rdc_fail;
+
+    function new;
+        this.rdc_success = $fopen("./rdc_success.txt", "w");
+        this.rdc_fail = $fopen("./rdc_fail.txt", "w");
+    endfunction
+
+    function logging(string func,string add, string msg);
+        if (func == add) begin
+            $fdisplay(this.rdc_success, "%s", msg);
+        end else begin
+            $fdisplay(this.rdc_success, "%s", msg);
+        end
+    endfunction
+    
+    function reset(string appendix);
+        case (appendix)
+            A : this.resetA;
+            B : this.resetB;
+            C : this.resetC;
+            default : $stop;
+        endcase
+    endfunction
+    
+    function resetA(string suffix);
+        {"\n\n\t\t".join(assertions["A"])}
+    endfunction
+
+    function resetB;
+        {"\n\n\t\t".join(assertions["B"])}
+    endfunction
+
+    function resetC;
+        {"\n\n\t\t".join(assertions["A"])}
+    endfunction
+endclass    
+"""
+    return template
+
+
+def main():
+    test = load_file("../test.txt")
+    test2 = sort_signals("top.test", test)
+    test3 = create_assertion(test2)
+    test4 = create_tb_class(test3)
+    print("hi")
+
+
+main()
