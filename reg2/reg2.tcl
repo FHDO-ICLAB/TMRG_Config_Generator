b168cb3fb82124d470480f1ff18662004b1b4c9b
LucasFHDortmund <78490283+LucasFHDortmund@users.noreply.github.com>
2024-02-20 23:44:03 +0100

    update

--- reg2/reg2.tcl
+++ reg2/reg2.tcl
@@ -1,5 +1,9 @@
 #set_param tcl.collectionResultDisplayLimit 0
 
+set rtl 1
+set synth 0
+set impl 0
+
 # Either 3 or 1
 set countVotingcells 1
 set checkTripledResetDomain 1
@@ -18,6 +22,12 @@ proc get_triplicated_and_not_triplicated {leafcells triplicated not_triplicated 
 	set not_triplicated_log [open $not_triplicated w]
 	set triplication_faults [open $triplication_faults w]
 
+    set reset_signals_log [open "./reset_signals.txt" w]
+    set clock_signals_log [open "./clock_signals.txt" w]
+
+    set reset_signals {}
+    set clock_signals {}
+
     foreach leafcell $leafcells {
         # Determine suffix of leafcell
         regexp {([A-C])_reg(?!.*_)} $leafcell -> leafcell_s
@@ -31,6 +41,8 @@ proc get_triplicated_and_not_triplicated {leafcells triplicated not_triplicated 
         set pin_c [get_pins -of_objects [get_cells $leafcell] -filter {DIRECTION == IN && IS_CLOCK == true}]
         # Determine net connected to clock pin
         set net_c [get_nets -of_objects [get_pins $pin_c]]
+        # Append clock net to list
+        lappend clock_signals $net_c
         # Determine clock net suffix
         regexp {.*([A-C])} $net_c -> net_c_s
         # Determine parent of clocking net
@@ -49,6 +61,8 @@ proc get_triplicated_and_not_triplicated {leafcells triplicated not_triplicated 
         if {$pin_rst ne ""} {
             # Determine net connected to rst pin
             set net_rst [get_nets -of_objects [get_pins $pin_rst]]
+            # Append reset net to list
+            lappend reset_signals $net_rst
             # FF may not have a net connected to rst pin
             if {$net_rst ne ""} {
                 # Determine rst net suffix
@@ -107,39 +121,51 @@ proc get_triplicated_and_not_triplicated {leafcells triplicated not_triplicated 
             } else {
                 puts "$leafcell has too many output connections, check logs for further details"
                 puts $triplication_faults "$name, $leafcell, \[[join $cells ", "]\]"   
-            }  
+            } 
         } else {
             # If not, add to "not_triplicated"
 			puts "$leafcell sadly is not triplicated"
             puts $not_triplicated_log "$name, $leafcell"         
         }
     }
+    
+    puts $clock_signals_log [join $clock_signals "\n"]
+    puts $reset_signals_log [join $reset_signals "\n"]
+
 	close $triplicated_log
 	close $not_triplicated_log
 	close $triplication_faults
+    close $clock_signals_log
+    close $reset_signals_log
+
+
+
 }
 
-#Opens Elaborated Design
-synth_design -rtl
-current_instance $hierarchy_lvl
-#Get all leafcells that are primitive and belong to RTL_REGISTER group
-set leafcells [lsort -unique -decreasing -dictionary [get_cells -hierarchical -filter {IS_PRIMITIVE == true && PRIMITIVE_GROUP == RTL_REGISTER}]]
-get_triplicated_and_not_triplicated $leafcells "./triplicated_rtl.txt" "./not_triplicated_rtl.txt" "./triplication_faults_rtl.txt"
-close_design
+if {$rtl} {
+    #Opens Elaborated Design
+    synth_design -rtl
+    current_instance $hierarchy_lvl
+    #Get all leafcells that are primitive and belong to RTL_REGISTER group
+    set leafcells [lsort -unique -decreasing -dictionary [get_cells -hierarchical -filter {IS_PRIMITIVE == true && PRIMITIVE_GROUP == RTL_REGISTER}]]
+    get_triplicated_and_not_triplicated $leafcells "./logs/rtl/triplicated_rtl.txt" "./logs/rtl/not_triplicated_rtl.txt" "./logs/rtl/triplication_faults_rtl.txt"
+    close_design
+} elseif {$synth} {
 
-#Opens Synthesized Design
-open_run synth_1 -name synth_1
-current_instance $hierarchy_lvl
-#Get all leafcells that are primitive and belong to FLOP_LATCH group
-set leafcells [lsort -unique -decreasing -dictionary [get_cells -hierarchical -filter {IS_PRIMITIVE == true && PRIMITIVE_GROUP =~ FLOP*}]]
-get_triplicated_and_not_triplicated $leafcells "triplicated_synth.txt" "not_triplicated_synth.txt" "./triplication_faults_synth.txt"
-close_design
+    #Opens Synthesized Design
+    open_run synth_1 -name synth_1
+    current_instance $hierarchy_lvl
+    #Get all leafcells that are primitive and belong to FLOP_LATCH group
+    set leafcells [lsort -unique -decreasing -dictionary [get_cells -hierarchical -filter {IS_PRIMITIVE == true && PRIMITIVE_GROUP =~ FLOP*}]]
+    get_triplicated_and_not_triplicated $leafcells "./logs/synth/triplicated_synth.txt" "./logs/synth/not_triplicated_synth.txt" "./logs/synth/triplication_faults_synth.txt"
+    close_design
+} elseif {$impl} {
 
-#Opens Synthesized Design
-open_run impl_1
-current_instance $hierarchy_lvl
-#Get all leafcells that are primitive and belong to FLOP_LATCH group
-set leafcells [lsort -unique -decreasing -dictionary [get_cells -hierarchical -filter {IS_PRIMITIVE == true && PRIMITIVE_GROUP =~ FLOP*}]]
-get_triplicated_and_not_triplicated $leafcells "triplicated_impl.txt" "not_triplicated_impl.txt" "./triplication_faults_impl.txt"
-close_design
-log_file -close
+    #Opens Synthesized Design
+    open_run impl_1
+    current_instance $hierarchy_lvl
+    #Get all leafcells that are primitive and belong to FLOP_LATCH group
+    set leafcells [lsort -unique -decreasing -dictionary [get_cells -hierarchical -filter {IS_PRIMITIVE == true && PRIMITIVE_GROUP =~ FLOP*}]]
+    get_triplicated_and_not_triplicated $leafcells "./logs/impl/triplicated_impl.txt" "./logs/impl/not_triplicated_impl.txt" "./logs/impl/triplication_faults_impl.txt"
+    close_design
+}
